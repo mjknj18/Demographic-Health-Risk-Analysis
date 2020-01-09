29f06173251ac516aa5e4a9aecd8a687e3c7f136
Matt Kennedy <mjknj18@gmail.com>
2020-01-08 20:23:46 -0500

    Updated JavaScript Code.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,28 @@
 // Define Plot Margins & Dimensions
 var width = parseInt(d3.select("#scatter").style("width"));
+
 // Designate the height of the graph
 var height = width - width / 3.9;
+
 // Margin spacing for graph
 var margin = 20;
+
 // space for placing words
 var labelArea = 110;
+
 // padding for the text at the bottom and left axes
 var tPadBot = 40;
 var tPadLeft = 40;
+
 var svg = d3
   .select("#scatter")
   .append("svg")
   .attr("width", width)
   .attr("height", height)
   .attr("class", "chart");
+
 var circRadius;
+
 function crGet() {
   if (width <= 530) {
     circRadius = 5;
@@ -24,11 +31,14 @@ function crGet() {
     circRadius = 10;
   }
 }
+
 crGet();
+
 d3.csv("https://raw.githubusercontent.com/mjknj18/Demographic-Health-Risk-Analysis/master/assets/data/data.csv").then(function(data) {
   // Visualize the data
   visualize(data);
 });
+
 function visualize(theData) {
   var curX = "poverty";
   var curY = "obesity";
@@ -36,6 +46,7 @@ function visualize(theData) {
   var xMax;
   var yMin;
   var yMax;
+
   function xMinMax() {
     xMin = d3.min(theData, function(d) {
       return parseFloat(d[curX]) * 0.90;
@@ -44,6 +55,7 @@ function visualize(theData) {
       return parseFloat(d[curX]) * 1.10;
     });
   }
+
   function yMinMax() {
     yMin = d3.min(theData, function(d) {
       return parseFloat(d[curY]) * 0.90;
@@ -52,6 +64,7 @@ function visualize(theData) {
       return parseFloat(d[curY]) * 1.10;
     });
   }
+
   function labelChange(axis, clickedText) {
     d3
       .selectAll(".aText")
@@ -61,18 +74,23 @@ function visualize(theData) {
       .classed("inactive", true);
     clickedText.classed("inactive", false).classed("active", true);
   }
+
   xMinMax();
   yMinMax();
+
   var xScale = d3
     .scaleLinear()
     .domain([xMin, xMax])
     .range([margin + labelArea, width - margin]);
+
   var yScale = d3
     .scaleLinear()
     .domain([yMin, yMax])
     .range([height - margin - labelArea, margin]);
+
   var xAxis = d3.axisBottom(xScale);
   var yAxis = d3.axisLeft(yScale);
+
   function tickCount() {
     if (width <= 500) {
       xAxis.ticks(5);
@@ -83,18 +101,23 @@ function visualize(theData) {
       yAxis.ticks(10);
     }
   }
+
   tickCount();
+
   svg
     .append("g")
     .call(xAxis)
     .attr("class", "xAxis")
     .attr("transform", "translate(0," + (height - margin - labelArea) + ")");
+
   svg
     .append("g")
     .call(yAxis)
     .attr("class", "yAxis")
     .attr("transform", "translate(" + (margin + labelArea) + ", 0)");
+
   var theCircles = svg.selectAll("g theCircles").data(theData).enter();
+  
   theCircles
     .append("circle")
     .attr("cx", function(d) {
